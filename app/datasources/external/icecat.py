51469e41565a505aa1912156de965f0ed23d66f7
bahag-fadinaa <adeolu-samuel.fadina@bahag.com>
2022-02-14 07:50:34 +0100

    Add icecat api

---
+++ app/datasources/external/icecat.py
@@ -0,0 +1,53 @@
+from typing import Union, List
+from fastapi.exceptions import HTTPException
+from httpx import AsyncClient
+
+from app.datasources.generic import DataSource
+
+
+class Icecat(DataSource):
+    def __init__(self):
+        self.session = AsyncClient(verify=False)
+
+
+    def _extract_attributes(data: dict):
+        product_info = {
+            "EAN": data.get("GeneralInfo").get("GTIN"),
+            "Title": data.get("GeneralInfo").get("Title"),
+            "Brand": data.get("GeneralInfo").get("Brand"),
+        }
+        feature_groups = data.get("FeaturesGroups")
+        for feature_group in feature_groups:
+            for feature in feature_group.get("Features"):
+                product_info.update(
+                    {
+                        feature.get("Feature").get("Name").get("Value"): feature.get("PresentationValue")
+                    }
+                )
+        return product_info
+
+
+    async def search(self, query: str) -> dict:
+        async with self.session as client:
+            api_url = f"https://live.icecat.biz/api/?UserName=openIcecat-live&Language=de&GTIN={query}&Content=All"
+            response = await client.get(api_url)
+            json_body = response.json()
+            if json_body.get("data"):
+                product_info = Icecat._extract_attributes(json_body.get("data"))
+                return product_info
+            elif json_body.get("StatusCode")==9:
+                return 0 # Product not available in open icecat but in full icecat.
+            else:
+                return [] # Product not availiable in icecat.
+
+
+    async def search_full_icecart(self, query: str, username, icecat_api_key) -> dict:
+        async with self.session as client:
+            api_url = f"https://live.icecat.biz/api/?shopname={username}&lang=en&content=&GTIN={query}&app_key={icecat_api_key}&Content=All"
+            response = await client.get(api_url)
+            json_body = response.json()
+            if json_body.get("data"):
+                product_info = product_info = Icecat._extract_attributes(json_body.get("data"))
+                return product_info
+            else:
+                return []
