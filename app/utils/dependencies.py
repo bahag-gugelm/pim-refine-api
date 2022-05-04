c429a60ca70901e42670cd64583686ebf893fb11
Mike Gugel <mikhail.gugel@bahag.com>
2022-05-04 13:47:02 +0200

    pim -> ean conversion moved from route to deps.

--- app/utils/dependencies.py
+++ app/utils/dependencies.py
@@ -1,8 +1,11 @@
-from fastapi import Depends
+import re
+
+from fastapi import Depends, HTTPException, status
 from fastapi_users.db import OrmarUserDatabase
 
 from app.models.user import UserDB, UserModel
 from app.db.managers import UserManager
+from app.datasources.internal.bdx_am import BdxAm
 
 
 
@@ -22,3 +25,31 @@ def get_current_user():
 def get_current_admin():
     from app.routers.users import fastapi_users
     return fastapi_users.current_user(superuser=True)
+
+
+async def get_eans(query: str):
+    query = re.findall(r'(\d+)', query)
+    query_items_length = set(len(num) for num in query)
+    if not query_items_length:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Empty query",
+            )
+    if len(query_items_length) > 1:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Mixed length items in query",
+            )
+    if query_items_length.pop() not in (13, ):
+        pim_client = BdxAm()
+        eans = list()
+        for num in query:
+            ean = await pim_client.pim2ean(num)
+            ean and eans.append(ean)
+        if not eans:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="No valid PIM_SKU found in query",
+                )
+        query = eans
+    return query
