7b07052404e0bd0cb7d665bf4171ddefd84d766f
Mike Gugel <mikhail.gugel@bahag.com>
2022-05-10 14:42:28 +0200

    More consistency in processing items with  ean length < 13 in pim.

--- app/datasources/internal/bdx_am.py
+++ app/datasources/internal/bdx_am.py
@@ -9,7 +9,7 @@ class BdxAm(DataSource):
             'Material_group', 'Variant_product',
             'Mandators', 'SAP_name', 'lfd_nr'
             }
-        res205 = await PimQuery20_5.objects.get_or_none(EAN=query)
+        res205 = await PimQuery20_5.objects.get_or_none(EAN=query.lstrip('0'))
         res = res205 and res205.dict()
         if res:
             res29 = await PimQuery29.objects.all(Variant_product=res['Variant_product'])
@@ -20,4 +20,5 @@ class BdxAm(DataSource):
     async def pim2ean(self, query: str):
         res = await PimQuery20_5.objects.get_or_none(Variant_product=query)
         if res:
-            return res.EAN
+            ean = res.EAN
+            return f'{"0"*(13-len(ean))}{ean}'
