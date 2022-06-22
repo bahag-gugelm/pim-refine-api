3fb0dce8d7ae6f0b4db6075ae665cf8600754710
Mike Gugel <mikhail.gugel@bahag.com>
2022-06-22 10:18:52 +0200

    id excluded from the response data

--- app/datasources/internal/bdx_am.py
+++ app/datasources/internal/bdx_am.py
@@ -8,7 +8,7 @@ class BdxAm(DataSource):
     @cached
     async def search(self, query: str):
         attribs_exclude_fields = {
-            'Material_group', 'Variant_product',
+            'id', 'Material_group', 'Variant_product',
             'Mandators', 'SAP_name',
             }
         res205 = await PimQuery20_5.objects.get_or_none(EAN=query.lstrip('0'))
